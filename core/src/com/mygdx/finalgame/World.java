c4d661c1f1ecb527f2c330dd71cf965fea81a2d5
vandl4973 <vandl4973@HRH-IC0027326.wrdsb.ca>
2018-01-10 10:03:02 -0500

    world is done for prototype

--- core/src/com/mygdx/finalgame/World.java
+++ core/src/com/mygdx/finalgame/World.java
@@ -5,10 +5,35 @@
  */
 package com.mygdx.finalgame;
 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+
 /**
  *
  * @author Adars
  */
 public class World {
-    
+    private Rectangle [] lasers;
+    private ShapeRenderer shape;
+  
+    public World(){
+        lasers = new Rectangle[4];
+        lasers[0]= new Rectangle(100,100,20,100);
+        lasers[1]=new Rectangle(110,110,30,110);
+        lasers[2]=new Rectangle(120,10,50,110);
+        lasers[3]=new Rectangle(50,300,60,130);   
+        shape = new ShapeRenderer();
+    }
+    public void render(){
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        for (int i = 0; i <lasers.length; i++) {
+            shape.rect(lasers[i].x, lasers[i].y, lasers[i].width, lasers[i].height);
+        }
+        shape.end();
+        
+    }
+    public Rectangle[] getLasers(){
+        return lasers;
 }
+}
+

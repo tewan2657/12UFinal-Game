6c5cedbf0e237a5b9111f6e3b67992eb8bb1d3dc
Adars <Adars@LAPTOP-5M1MF8QA>
2018-01-11 09:34:29 -0500

    Player class is done. All we need to do is comment it. Player is moving vertically as we wanted.

--- core/src/com/mygdx/finalgame/Player.java
+++ core/src/com/mygdx/finalgame/Player.java
@@ -20,96 +20,94 @@ import com.badlogic.gdx.utils.Array;
  * @author Adars
  */
 public class Player {
+
     private float x;
     private float y;
-    
+
     private float dx;
     private float dy;
-    
+
     private boolean facingLeft;
-    
+
     private float elapsed;
-    
+
     private Animation<TextureRegion> run;
     private Animation<TextureRegion> runL;
-    
+
     private TextureRegion stand;
     private TextureRegion standL;
-    
+
     private TextureAtlas atlas;
-    
-    public Player(float x, float y){
-        
-        this.x = x; 
+
+    public Player(float x, float y) {
+
+        this.x = x;
         this.y = y;
-        
+
         this.dx = 0;
         this.dy = 0;
-        
+
         this.elapsed = 0;
-        
+
         this.atlas = new TextureAtlas("packed/player.atlas");
-        
+
         this.stand = atlas.findRegion("stand");
         this.standL = new TextureRegion(stand);
         this.standL.flip(true, false);
-        
+
         run = new Animation(1f / 10f, atlas.findRegions("run"));
-        
-        
+
         Array<AtlasRegion> runLFrames = atlas.findRegions("run");
         for (int i = 0; i < runLFrames.size; i++) {
             runLFrames.get(i).flip(true, false);
         }
-        runL = new Animation(1f/10f, runLFrames);
-        
+        runL = new Animation(1f / 10f, runLFrames);
+
         this.facingLeft = false;
-        
-    
+
     }
-    
-    public float getX(){
+
+    public float getX() {
         return x;
     }
-    
-    public float getY(){
-     return y;   
+
+    public float getY() {
+        return y;
     }
-     public void update(float deltaTime){
-         if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
-             this.dy = 3;
-             this.elapsed = this.elapsed + deltaTime;
-             this.facingLeft = false;
-         }else{
-             this.dx = 0;
-             this.elapsed = 0;
-         }
-         this.y = this.y + this.dy;
-             
-     }
-     
-     public void render(SpriteBatch batch){
-         
-      if(this.dy == 0){   
-          if(facingLeft){
-              batch.draw(standL, x, y);
-          }else{
-              batch.draw(stand, x, y);
-          }
-      }else if (this.dx > 0){
-          batch.draw(run.getKeyFrame(elapsed,true), x, y);
-          
-      }else if (this.dx < 0) {
-          batch.draw(runL.getKeyFrame(elapsed,true), x, y);
-      }
-     }
-     
-     
-     public void dispose(){
-         atlas.dispose();
-     }
-     
-     
-     
-    
+
+    public void update(float deltaTime) {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            this.dy = +3;
+            this.elapsed = this.elapsed + deltaTime;
+            this.facingLeft = false;
+
+        } else {
+            this.dy = -3;
+            this.elapsed = this.elapsed + deltaTime;
+        }
+
+        this.y = this.y + this.dy;
+
+    }
+
+    public void render(SpriteBatch batch) {
+
+        if (this.dx == 0) {
+            if (facingLeft) {
+                batch.draw(standL, x, y);
+            } else {
+                batch.draw(stand, x, y);
+            }
+        } else if (this.dx > 0) {
+            batch.draw(run.getKeyFrame(elapsed, true), x, y);
+
+        } else if (this.dx < 0) {
+            batch.draw(runL.getKeyFrame(elapsed, true), x, y);
+        }
+    }
+
+    public void dispose() {
+        atlas.dispose();
+    }
+
 }

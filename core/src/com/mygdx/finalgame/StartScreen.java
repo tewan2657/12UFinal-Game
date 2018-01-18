b5c9aec911a44dd75467967fedeb444c52c07a32
vandl4973 <vandl4973@HRH-IC0027326.wrdsb.ca>
2018-01-18 10:06:59 -0500

    added class to make main menu

---
+++ core/src/com/mygdx/finalgame/StartScreen.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.finalgame;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ *
+ * @author vandl4973
+ */
+public class StartScreen implements Screen {
+
+    @Override
+    public void show() {
+        
+         }
+
+    @Override
+    public void render(float f) {
+        
+        }
+
+    @Override
+    public void resize(int i, int i1) {
+        }
+
+    @Override
+    public void pause() {
+        }
+
+    @Override
+    public void resume() {
+        }
+
+    @Override
+    public void hide() {
+        }
+
+    @Override
+    public void dispose() {
+        }
+    
+}

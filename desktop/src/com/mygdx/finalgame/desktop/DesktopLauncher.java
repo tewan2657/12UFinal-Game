ee13ab2c852a1eef6ec0d7e4bc170dd3d2e47cf4
Adars <Adars@LAPTOP-5M1MF8QA>
2018-01-24 09:55:43 -0500

    GAME IS DONE !!!

--- desktop/src/com/mygdx/finalgame/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/finalgame/desktop/DesktopLauncher.java
@@ -3,8 +3,8 @@ package com.mygdx.finalgame.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
-
 import com.mygdx.finalgame.finalgame;
+
 public class DesktopLauncher {
 
     public static void main(String[] arg) {
